bf6a75eddb7fffad5a4bdbf37f5bc39ad80b316b
wisanu <wisanu.pho@cru.ac.th>
2022-07-26 14:03:28 +0700

    function

---
+++ function.py
@@ -0,0 +1,12 @@
+#สร้างฟังก์ชั่น
+def hello():
+    print("Hello world")
+    print("Wisanu")
+#เรียกใช้งานฟังก์ชั่น
+hello()
+#ส่งค่าผ่านparameter+return
+def sum(x,y):
+    x+y
+    return "hello"
+
+print(sum(5,8))
